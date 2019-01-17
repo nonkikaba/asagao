dc9a8c58bc0edd74e6f84e70b8bfa83807306fb2
nonkikaba <rakurakuhuton.20011@gmail.com>
2019-01-17 13:24:17 +0900

    0117

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,7 @@ Rails.application.routes.draw do
     resource :session, only: [:create, :destroy]
     resource :account, only: [:show, :edit, :update]
     resource :password, only: [:show, :edit, :update]
+
+    resources :articles
   end
 end

dd030b18386464e427e59de81896a9040592651a
nonkikaba <rakurakuhuton.20011@gmail.com>
2019-01-26 15:35:05 +0900

    01261535

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,70 +1,17 @@
 class ArticlesController < ApplicationController
   before_action :login_required, except: [:index, :show]
 
+  # 記事一覧
   def index
-    @articles = Article.order(released_at: :desc)
+    @articles = Article.visible.order(released_at: :desc)
     @articles = @articles.open_to_the_public unless current_member
-
-    unless current_member&.administrator?
-      @articles = @articles.visible
-    end
-
     @articles = @articles.page(params[:page]).per(5)
   end
 
+  # 記事詳細
   def show
-    articles = Article.all
-
+    articles = Article.visible
     articles = articles.open_to_the_public unless current_member
-
-    unless current_member&.administrator?
-      articles  = articles.visible
-    end
-
-    @article = Article.find(params[:id])
-  end
-
-  def new
-    @article = Article.new
-  end
-
-  def create
-    @article = Article.new(article_params)
-    if @article.save
-      redirect_to @article, notice: "ニュース記事を登録しました"
-    else
-      render "new"
-    end
-  end
-
-  def edit
-    @article = Article.find(params[:id])
-  end
-
-  def update
-    @article = Article.find(params[:id])
-    @article.assign_attributes(article_params)
-    if @article.save
-      redirect_to @article, notice: "ニュース記事を更新しました。"
-    else
-      render "edit"
-    end
-  end
-
-  def destroy
-    @article = Article.find(params[:id])
-    @article.destroy
-    redirect_to :articles
-  end
-
-  private def article_params
-    params.require(:article).permit(
-      :title,
-      :body,
-      :released_at,
-      :no_expiration,
-      :expired_at,
-      :member_only
-    )
+    @article = articles.find(params[:id])
   end
 end
